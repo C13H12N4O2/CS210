19509bece5a613c8d315ec72aa7bad15f8fc744a
C13H12N4O2 <c13h12n4o2@gmail.com>
2020-09-19 23:18:30 +0900

    Update Percolation

--- Project/Percolation/src/Percolation.java
+++ Project/Percolation/src/Percolation.java
@@ -3,41 +3,71 @@ import stdlib.In;
 import stdlib.StdOut;
 
 public class Percolation {
-    ...
-
+    WeightedQuickUnionUF uf;
+    boolean[][] perc;
+    int openSites;
+    int N;
+    int in;
+    int out;
+    
     // Constructs an n x n percolation system, with all sites blocked.
     public Percolation(int n) {
-        ...
+        perc = new boolean [n][n];
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        openSites = 0;
+        N = n;
+        in = 0;
+        out = N * N + 1;
+        
+        for (int i = 0; i != N + 1; i++) {
+            uf.union(encode(in, i), in);
+            uf.union(encode(N - 1, i), out);
+        }
+        
     }
 
     // Opens site (i, j) if it is not already open.
     public void open(int i, int j) {
-        ...
+        if (!isOpen(i, j)) {
+            perc[i][j] = true;
+            ++openSites;
+            
+            if (i > 0 && isOpen(i - 1, j))
+                uf.union(encode(i, j), encode(i - 1, j));
+            if (i < N - 1 && isOpen(i + 1, j))
+                uf.union(encode(i, j), encode(i + 1, j));
+            if (j > 0 && isOpen(i, j - 1))
+                uf.union(encode(i, j), encode(i, j - 1));
+            if (j < N - 1 && isOpen(i, j + 1))
+                uf.union(encode(i, j), encode(i, j + 1));
+        }
     }
 
     // Returns true if site (i, j) is open, and false otherwise.
     public boolean isOpen(int i, int j) {
-        ...
+        return perc[i][j];
     }
 
     // Returns true if site (i, j) is full, and false otherwise.
     public boolean isFull(int i, int j) {
-        ...
+        if (percolates())
+            return true;
+        return false;
     }
 
     // Returns the number of open sites.
     public int numberOfOpenSites() {
-        ...
+        return openSites;
     }
 
     // Returns true if this system percolates, and false otherwise.
     public boolean percolates() {
-        ...
+        return uf.connected(in, out);
     }
 
     // Returns an integer ID (1...n) for site (i, j).
     private int encode(int i, int j) {
-        ...
+        return i * N + j + 1;
     }
 
     // Unit tests the data type. [DO NOT EDIT]
